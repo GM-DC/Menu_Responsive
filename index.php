781f11ca073eb10aaaef5670093b151628bcd18f
GM-DC <75643164+GM-DC@users.noreply.github.com>
2022-05-15 17:47:06 -0500

    MenuLateral v0.1

---
+++ index.php
@@ -0,0 +1,76 @@
+<?php include("./View/head.php"); ?>
+
+    <header>
+        <div class="icon_menu">
+            <i class="fa-solid fa-bars" title="Menú" id="btn_open"></i>
+        </div>
+    </header>
+
+    <div class="menu__side" id="menu_side">
+        
+        <div class="name__page">
+            <i class="fa-solid fa-coins" ></i>
+            <h4>GIAN </h4>
+        </div>
+
+        <!-- Opciones -->
+        <div class="options__menu">
+
+            <a href="#" class="selected">
+                <div class="option">
+                    <i class="fa-solid fa-house-user"></i>
+                    <h4 title="Inicio">Inicio</h4>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="option">
+                    <i class="fa-solid fa-toilet-portable"></i>
+                    <h4>Portafolio</h4>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="option">
+                    <i class="fa-solid fa-fire-burner"></i>
+                    <h4>Cursos</h4>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="option">
+                    <i class="fa-solid fa-chart-simple"></i>
+                    <h4>Blog</h4>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="option">
+                    <i class="fa-solid fa-trowel-bricks"></i>
+                    <h4>Contacto</h4>
+                </div>
+            </a>
+
+        </div>
+        
+    </div>
+
+    <main>
+        
+        <h1> TABLA DE CONSULTA</h1><br>
+        
+        <P>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe enim nam odio quia et, suscipit minus quae cupiditate quasi optio facere, deserunt veniam soluta blanditiis nihil veritatis tenetur atque sequi?
+            <br>
+        </P>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate, ea error maiores laboriosam amet ad labore illo possimus cumque eaque expedita perferendis doloremque atque sit dignissimos consectetur deleniti alias consequuntur!
+        Iusto laudantium voluptatum doloribus non recusandae molestiae veritatis commodi iure officia labore deserunt, quam quae ex tempora quis asperiores nisi debitis. Culpa omnis mollitia consequuntur nesciunt, illo voluptates voluptatem aperiam!
+        Esse tempora non vitae, cupiditate corrupti quibusdam alias molestias distinctio! In doloremque ducimus minima dignissimos, fugit repellendus natus magni nostrum quis rem cupiditate odio, perspiciatis similique suscipit consectetur repellat cumque!
+    </p>
+    </main>
+
+
+
+
+
+<?php include("./View/body.php"); ?>
